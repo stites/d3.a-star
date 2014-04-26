c67f65f5c5675a45e9670ccd135534a326c14e24
Sam Stites <stites.sr@gmail.com>
2014-04-26 01:09:43 -0700

    we have random maps!

--- astar.js
+++ astar.js
@@ -5,11 +5,12 @@ var w = window.innerWidth,
     unitLen = 20,
     border = 2,
     side = unitLen - border,
+    wallPercent = 4;
     n = ~~(w / side),
     m = ~~(h / side);
 
 var c = colors = {
-  empty: '#fff',
+  empty: '#FFF',
   wall: '#4682B4',
   border: '#bbb',
 }
@@ -35,16 +36,23 @@ var map = d3.select('body')
             });
 
 var units = map.selectAll('span')
-               .data(getUnits(n,m))
-               .enter().append('svg:rect')
-                 .attr({
-                   x: function(d){return d.x * unitLen},
-                   y: function(d){return d.y * unitLen},
-                   width:  side,
-                   height: side,
-                   fill: c.wall,
-                   class: 'unit',
-                 });
+              .data( getUnits(n,m).map(function(d, idx, cxt){
+                  d.wall = ~~(rand(wallPercent)) === 0;
+                  return d;
+                })
+              )
+              .enter().append('svg:rect')
+                .attr({
+                  x: function(d){return d.x * unitLen},
+                  y: function(d){return d.y * unitLen},
+                  width:  side,
+                  height: side,
+                  fill: function(d){
+                    // console.log(d)
+                    return d.wall ? c.wall : c.empty;
+                  },
+                  class: 'unit',
+                });
 
 
 
