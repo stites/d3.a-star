791a1c3e44c5fee41bf14edb433584d81194233b
Sam Stites <stites.sr@gmail.com>
2014-04-25 23:47:51 -0700

    added util functions to generate map coordinates

--- astar.js
+++ astar.js
@@ -1,25 +1,38 @@
+
+/* VARIABLES */
 var w = window.innerWidth,
     h = window.innerHeight,
-    side = 40,
+    side = 20,
     n = ~~(w / side),
     m = ~~(h / side);
 
+/* UTIL FUNCTIONS */
 var rand = function(n){ return Math.random() * n; };
+var getUnits = function(n,m){
+  var units = [];
+  for (var i = 0; i < n; i++){
+    for (var j = 0; j < m; j++){
+      units.push({ x:i, y:j })
+    }
+  }
+  return units;
+}
 
 var map = d3.select('body')
             .append('svg')
             .attr({
               width: w,
               height: h,
-              color: '#88888'
+              class: 'map',
             });
 
+
 var unit = map.selectAll('span')
-              .data(d3.range(n*m))
+              .data(getUnits(n,m))
               .enter().append('svg:rect')
                 .attr({
-                  x: function(d, i){return d * 40},
-                  y: function(d, i){return d * 40},
+                  x: function(d, i){return d.x * side},
+                  y: function(d, i){return d.y * side},
                   width:  side,
                   height: side,
                   class: 'unit',
